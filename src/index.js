b11d7b670fb073b6487b8a58c798325b069d6160
Tobias Nyholm Maneschijn <tfrom222@gmail.com>
2021-04-09 20:18:48 +0200

    added firebase

--- src/index.js
+++ src/index.js
@@ -5,6 +5,20 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
+import firebase from "firebase/app";
+
+// Use your config values here.
+firebase.initializeApp({
+  apiKey: "AIzaSyDejPOfp-0o50T7k2vFaYNKf9ReBNPJN7g",
+  authDomain: "stask-fba8c.firebaseapp.com",
+  projectId: "stask-fba8c",
+  storageBucket: "stask-fba8c.appspot.com",
+  messagingSenderId: "995153194317",
+  appId: "1:995153194317:web:6fe41d57054e70422edb04",
+  measurementId: "G-KYZC0RWRBN"
+});
+
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
@@ -14,3 +28,5 @@ ReactDOM.render(
   document.getElementById("root")
 );
 reportWebVitals();
+
+
